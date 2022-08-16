87583644406ee1773c567be0a37cdb4bd69d4311
radja <rhriman869@gmail.com>
2022-08-16 23:06:23 +0100

    commit

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    path('register/', register),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('companylist/',company_list),
